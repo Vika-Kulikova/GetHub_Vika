6f6596e317abfe46ef2c6f35440a3683d8074f8b
Vika-Kulikova <visiabez@gmail.com>
2019-01-25 00:02:01 +0200

    added few instances of the class

--- test_issue_V/scripts/person.js
+++ test_issue_V/scripts/person.js
@@ -1,56 +1,71 @@
 class PersonProp {
-    constructor(name, age, weight, height) {
-        this.name = name;
-        this.age = age;
-        this.weight = weight;
-        this.height = height;
-    }
-
-    getName(){
-        let name = prompt('Enter your name, please', '');
-        return this.name = name;
-    }
-
-    getAge(){
-        let age = prompt('Enter your age, please', '');
-        return this.age = age;
-    }
-    getWeight(){
-        let weight = prompt('Enter your weight, please', '');
-        this.weight = weight;
-    }
-    getHeight(){
-        let height = prompt('Enter your height in Mts, please', '');
-        this.height = height;
-    }
-}
-
-let personProp = new PersonProp();
-
-class Person {
-
-    // constructor(name = "", age = "0", DNI, sex = "M", weight = 0, height = 0) {
+    // constructor(name, age, sex, weight, height) {
     //     this.name = name;
     //     this.age = age;
-    //     this.DNI = DNI;
     //     this.sex = sex;
     //     this.weight = weight;
     //     this.height = height;
     // }
 
-    constructor(name, age, DNI=0, sex, weight = 0, height = 0) {
-        this.name = name;
-        this.age = age;
-        this.DNI = DNI;
-        this.sex = sex;
-        this.weight = weight;
-        this.height = height;
+    constructor() {
     }
 
-    // constructor(name, age, DNI, sex, weight, height) {
+    name() {
+        let name = prompt('Enter your name, please', '');
+        return name;
+    }
+
+    age() {
+        let age = +prompt('Enter your age, please', '');
+        return age;
+    }
+
+    sex() {
+        let sex = prompt('Enter your sex, please', 'F/M');
+        return sex;
+    }
+
+    weight() {
+        let weight = +prompt('Enter your weight, please', '');
+        return weight;
+    }
+
+    height() {
+        let height = +prompt('Enter your height in Mts, please', '');
+        return height;
+    }
+}
+
+let personProp = new PersonProp();
+let name = personProp.name();
+let age = personProp.age();
+let sex = personProp.sex();
+let weight = personProp.weight();
+let height = personProp.height();
+
+
+
+class Person {
+
+    constructor(name = "", age = "0", sex = "M", weight = 0, height = 0) {
+        this._name = name;
+        this._age = age;
+        this._sex = sex;
+        this._weight = weight;
+        this._height = height;
+    }
+
+    // constructor(name, age, sex, weight = 0, height = 0) {
+    //     this._name = name;
+    //     this._age = age;
+    //     this._sex = sex;
+    //     this._weight = weight;
+    //     this._height = height;
+    // }
+
+    // constructor(name, age, sex, weight, height) {
     //     this.name = name;
     //     this.age = age;
-    //     this.DNI = DNI;
     //     this.sex = sex;
     //     this.weight = weight;
     //     this.height = height;
@@ -58,7 +73,11 @@ class Person {
 
 
     calculateIMC() {
-        let result = this.weight / Math.pow(this.height, 2);
+        if (this._weight <= 0  || this._height <= 0) {
+            throw 'Weight or height can\'t be <=0';
+        }
+        let result = this._weight / Math.pow(this._height, 2);
+
         if (result < 20) {
             return console.log(-1);
         } else if (20 < result && result < 25) {
@@ -68,62 +87,78 @@ class Person {
     }
 
     isAdult() {
-        if (this.age >= 18) {
-            return console.log(1);
+        if (this._age <= 0) {
+            throw 'Age can\'t be <=0';
         }
-        return console.log(0);
+        this._age >= 18 ? console.log(1) : console.log(0);
     }
 
     checkSex(sex) {
-        if (sex !== 'F' && sex !== 'M') {
-            this.sex = 'M';
-        }
-        return console.log(this.sex);
+        this._sex = (sex !== 'F' && sex !== 'M') ? 'M' : sex;
     }
-
 
     toString() {
         return console.log(JSON.stringify(this));
     }
 
     createDNI() {
-        if (this.DNI === null) {
-            let dni = Math.floor(10000000 + Math.random() * 90000000);
+        this._DNI = Math.floor(10000000 + Math.random() * 90000000);
 
-            //to make sure that the generated code is unique
-            while (dni_s.indexOf(dni) !== -1) {
-                dni = Math.floor(10000000 + Math.random() * 90000000);
-            }
-
-            dni_s.push(dni);
-            this.DNI = dni;
-            console.log(this.DNI)
-        }
-
-        console.log(this.DNI)
+        console.log(this._DNI)
     }
 
     set name(name) {
-        this.name = name;
+        this._name = name;
     }
+
     set age(age) {
-        this.age = age;
+        this._age = age;
     }
+
     set sex(sex) {
-        this.sex = sex;
+        this._sex = sex;
     }
+
     set weight(weight) {
-        this.weight = weight;
+        this._weight = weight;
     }
-    set height(weight) {
-        this.height = weight;
+
+    set height(height) {
+        this._height = height;
     }
 }
 
 
-let person = new Person();
-person.createDNI();
-person.calculateIMC();
-person.isAdult();
-person.checkSex(person.sex);
-person.toString();
+let person_first = new Person();
+person_first.createDNI();
+
+person_first.name = name;
+person_first.age = age;
+person_first.sex = sex;
+person_first.weight = weight;
+person_first.height = height;
+
+
+person_first.calculateIMC();
+person_first.isAdult();
+person_first.checkSex();
+person_first.toString();
+
+let person_second = new Person();
+person_second.createDNI();
+person_second.name = name;
+person_second.age = age;
+person_second.sex = sex;
+person_second.calculateIMC();
+person_second.isAdult();
+person_second.checkSex();
+person_second.toString();
+
+let person_third = new Person();
+person_third.createDNI();
+person_third.calculateIMC();
+person_third.isAdult();
+person_third.checkSex();
+person_third.toString();
+
+
--- test_issue_V/scripts/script.js
+++ test_issue_V/scripts/script.js
@@ -1,40 +1,41 @@
-function checkPass()
-{
-    let pass1 = document.getElementById('password');
-    let pass2 = document.getElementById('password-copy');
-    let message = document.getElementById('error-nwl');
-    let errorMassegColor = "pink";
+;(function () {
+        function checkPass() {
+            let pass1 = document.getElementById('password');
+            let pass2 = document.getElementById('password-copy');
+            let message = document.getElementById('error-nwl');
+            let errorMassegColor = "pink";
 
-    if(pass1.value == pass2.value){
-         message.innerHTML = "ok!";
+            if (pass1.value == pass2.value) {
+                message.innerHTML = "ok!";
 
-}
-    else{
-        pass2.style.backgroundColor = errorMassegColor;
-        message.style.color = errorMassegColor;
-        message.innerHTML = " These passwords don't match"
+            } else {
+                pass2.style.backgroundColor = errorMassegColor;
+                message.style.color = errorMassegColor;
+                message.innerHTML = " These passwords don't match"
+            }
+        }
+
+        function sendForm() {
+            alert('The date have been submitet');
+        }
+
+        function init() {
+            checkPass();
+            sendForm();
+
+        }
+
+        function checkForm() {
+            var button = document.getElementById('submit');
+            button.addEventListener("click", init);
+        }
+
+
+        try {
+            checkForm();
+        } catch (e) {
+            console.error(e)
+            alert('Error')
+        }
     }
-}  
-
-function sendForm() {
-    alert ('The date have been submitet');
-}
-
-function init () {
-    checkPass();
-    sendForm();
-
-}
-
-function checkForm() {
-    var button = document.getElementById('submit');
-    button.addEventListener("click", init);
-}
-
-
-try {
-    checkForm();
-} catch (e) {
-    console.error(e)
-    alert('Error')
-}
+)();
